f4f36e4ce0c93d9929c0362c6b00693b1afa4b7f
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-10-08 23:10:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,13 @@
 # Your code goes here!
 class Anagram
+  attr_accessor : word 
   
+  def initialize(word)
+  @word = word
+end
   
-  
+  def self.match 
+    self. each do |match|
+      
+  end
 end
